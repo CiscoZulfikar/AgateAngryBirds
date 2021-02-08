9115f883196d1e4269b470fee754fe5428d6ef6b
CiscoZ073 <13518073@std.stei.itb.ac.id>
2021-02-08 23:12:28 +0700

    added exploding bird

---
+++ Assets/Scripts/ExplodyBird.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodyBird : Bird
+{
+    public bool _hasExplode = false;
+
+    public void Explode()
+    {
+        if (State == BirdState.HitSomething && !_hasExplode)
+        {
+            transform.localScale = new Vector2(2.0f, 2.0f);
+            _hasExplode = true;
+        }
+    }
+
+    public override void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("BOOM");
+        State = BirdState.HitSomething;
+        Explode();
+    }
+}
